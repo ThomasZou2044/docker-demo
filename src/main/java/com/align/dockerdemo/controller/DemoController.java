371eced98938a47e01f5eae18a9188e5c2002596
JackZouShao <jackzouyan@gmail.com>
2023-07-10 17:09:21 +0900

    github-demo

---
+++ src/main/java/com/align/dockerdemo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.align.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yanzou
+ * @version V1.0.0
+ * @date 2023/07/10
+ * @description
+ **/
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo(){
+        return "Hello World";
+    }
+}
